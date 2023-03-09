43bf2105f42317db6db477d3d6c037b939608a7b
Stacy Vana <98608384+ssvana83@users.noreply.github.com>
2023-03-09 10:36:49 -0500

    DONE

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,13 @@
 class Person
-  # your code here
+  
+  attr_accessor :name, :weight, :wrist_size
+
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
+
+
 end
